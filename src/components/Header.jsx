97319f9a39886393e00f5d5ce5376bf43300a62a
ViniUme <viniciusumetsu210@gmail.com>
2022-02-07 13:50:50 -0300

    fix header images error

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import './styles/Header.css'
-import Logo from './img/logo.png'
-import Profile from './img/profile-icon.jpg'
+import './styles/Header.css';
 
 export default ({color}) => {
     return(
         <header className={color}>
-            <img className="Header--logo" src={Logo} alt="logo" />
-            <img className="Header--profile" src={Profile} alt="profile-icon" />
+            <img className="Header--logo" src="img/logo.png" alt="logo" />
+            <img className="Header--profile" src="img/profile-icon.jpg" alt="profile-icon" />
         </header>
     );
 }
